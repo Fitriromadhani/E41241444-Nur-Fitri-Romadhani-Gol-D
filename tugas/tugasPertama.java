efba8869aed09618564b2ce89fb44daaadf6afb6
Fitriromadhani <fitriromadhani.nfr@gmail.com>
2025-02-13 08:37:05 +0700

    Create tugasPertama.java

---
+++ tugas/tugasPertama.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas;
+
+/**
+ *
+ * @author user
+ */
+public class tugasPertama {
+    public static void main(String[] args) {
+        
+        String fName = "Lisa";
+        String IName = "Palombo";
+        String stuId = "1123456789";
+        String stuStatus = "Active";
+        
+        System.out.println("Student Name: " + fName + " " + IName);
+        System.out.println("Student ID: " + stuId);
+        System.out.println("Student Status: " + stuStatus);
+    }
+}
